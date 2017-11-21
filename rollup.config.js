e2899bc9cc77e8448f8f17b7c2ffcc815e6b7e74
Erik Koopmans <erik@erik-koopmans.com>
2017-11-21 22:09:11 +1100

    Fix Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,22 +6,44 @@ import pkg from './package.json';
 export default [
   // Bundled builds.
   {
+    name: 'html2pdf',
     input: 'src/index.js',
     output: [
       { file: pkg.main.replace(/js$/, 'bundle.js'), format: 'cjs' },
       { file: pkg.module.replace(/js$/, 'bundle.js'), format: 'es' },
       { file: pkg.browser.replace(/js$/, 'bundle.js'), format: 'iife' },
       { file: pkg.browser.replace(/js$/, 'umd.bundle.js'), format: 'umd' }
+    ],
+    globals: {
+      jspdf: 'jsPDF',
+      html2canvas: 'html2canvas'
+    },
+    plugins: [
+      resolve(),
+      commonjs()
     ]
   },
   // Un-bundled builds.
   {
+    name: 'html2pdf',
     input: 'src/index.js',
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
       { file: pkg.browser, format: 'iife' },
       { file: pkg.browser.replace(/js$/, 'umd.js'), format: 'umd' }
+    ],
+    external: [
+      'jspdf',
+      'html2canvas'
+    ],
+    globals: {
+      jspdf: 'jsPDF',
+      html2canvas: 'html2canvas'
+    },
+    plugins: [
+      resolve(),
+      commonjs()
     ]
   }
 ];
