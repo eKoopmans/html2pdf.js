d8a67bb0fd9702b8363ad216c3bf858e871769a6
rgrange <rgrange@vente-privee.com>
2017-12-26 18:11:10 +0100

    Add an option `opt.save` function wich replace the `pdf.save(opt.filename)` in order to be able to export the pdf the way we want. For example, with this solution, you can define the props save method like this : `save(pdf: any) {   resolve(pdf.output('blob'), fileName)); }`

--- dist/require/html2pdf.cjs.js
+++ dist/require/html2pdf.cjs.js
@@ -368,7 +368,11 @@ html2pdf.makePDF = function (canvas, pageSize, opt) {
   }
 
   // Finish the PDF.
-  pdf.save(opt.filename);
+  if (opt.save) {
+    opt.save(pdf);
+  } else {
+    pdf.save(opt.filename);
+  }
 };
 
 module.exports = html2pdf;
