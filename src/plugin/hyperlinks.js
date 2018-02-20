db96466fd587466e15bca584308dd75cfe4de379
Erik Koopmans <erik@erik-koopmans.com>
2018-02-21 01:57:46 +1100

    Split hyperlink plugin into two pieces

--- src/plugin/hyperlinks.js
+++ src/plugin/hyperlinks.js
@@ -1,19 +1,24 @@
 import Worker from '../worker.js';
 import { unitConvert } from '../utils.js';
 
+// Add hyperlink functionality to the PDF creation.
+
+// Main link array, and refs to original functions.
+var linkInfo = [];
 var orig = {
+  toContainer: Worker.prototype.toContainer,
   toPdf: Worker.prototype.toPdf,
 };
 
-Worker.prototype.toPdf = function toPdf() {
-  // Add hyperlink functionality to the PDF creation.
-  return orig.toPdf.call(this).then(function() {
-    // Add hyperlinks if the option is enabled.
+Worker.prototype.toContainer = function toContainer() {
+  return orig.toContainer.call(this).then(function toContainer_hyperlink() {
+    // Retrieve hyperlink info if the option is enabled.
     if (this.opt.enableLinks) {
       // Find all anchor tags and get the container's bounds for reference.
       var container = this.prop.container;
       var links = container.querySelectorAll('a');
       var containerRect = unitConvert(container.getBoundingClientRect(), this.prop.pageSize.k);
+      linkInfo = [];
 
       // Loop through each anchor tag.
       Array.prototype.forEach.call(links, function(link) {
@@ -28,13 +33,26 @@ Worker.prototype.toPdf = function toPdf() {
           var top = this.opt.margin[0] + clientRect.top % this.prop.pageSize.inner.height;
           var left = this.opt.margin[1] + clientRect.left;
 
-          this.prop.pdf.setPage(page);
-          this.prop.pdf.link(left, top, clientRect.width, clientRect.height, { url: link.href });
+          linkInfo.push({ page, top, left, clientRect, link });
         }
-      });
+      }, this);
+    }
+  });
+};
+
+Worker.prototype.toPdf = function toPdf() {
+  return orig.toPdf.call(this).then(function toPdf_hyperlink() {
+    // Add hyperlinks if the option is enabled.
+    if (this.opt.enableLinks) {
+      // Attach each anchor tag based on info from toContainer().
+      linkInfo.forEach(function(l) {
+        this.prop.pdf.setPage(l.page);
+        this.prop.pdf.link(l.left, l.top, l.clientRect.width, l.clientRect.height,
+                           { url: l.link.href });
+      }, this);
 
       // Reset the active page of the PDF to the final page.
-      var nPages = this.prop.pdf.putTotalPages().internal.getNumberOfPages();
+      var nPages = this.prop.pdf.internal.getNumberOfPages();
       this.prop.pdf.setPage(nPages);
     }
   });
