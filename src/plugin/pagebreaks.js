36041a97bc188172df61f3d8585bc2d9e2fe4c4c
Erik Koopmans <erik@erik-koopmans.com>
2018-09-11 22:30:50 +1000

    Improve page-break commenting

--- src/plugin/pagebreaks.js
+++ src/plugin/pagebreaks.js
@@ -1,14 +1,19 @@
 import Worker from '../worker.js';
 
+// Add page-break functionality.
+
+// Refs to original functions.
 var orig = {
   toContainer: Worker.prototype.toContainer
 };
 
 Worker.prototype.toContainer = function toContainer() {
   return orig.toContainer.call(this).then(function toContainer_pagebreak() {
-    // Enable page-breaks.
+    // Find all page-break elements and setup page height.
     var pageBreaks = this.prop.container.querySelectorAll('.html2pdf__page-break');
     var pxPageHeight = this.prop.pageSize.inner.px.height;
+
+    // Set each page-break element to a block with the appropriate height.
     Array.prototype.forEach.call(pageBreaks, function pageBreak_loop(el) {
       el.style.display = 'block';
       var clientRect = el.getBoundingClientRect();
