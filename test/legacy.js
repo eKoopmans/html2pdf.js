2bfbdf712846b77cefa18cf2b8d3ef9ef6f6e98e
Erik Koopmans <erik@erik-koopmans.com>
2019-07-10 00:05:17 +1000

    Convert stubbed save method into a chai-spy

--- test/legacy.js
+++ test/legacy.js
@@ -1,9 +1,15 @@
 describe('legacy mode', function () {
-  // Disable the save functionality.
-  html2pdf.Worker.prototype.save = function () {return this.then(function () {})};
+  // Stub the save functionality.
+  beforeEach(function () {
+    chai.spy.on(html2pdf.Worker.prototype, 'save', function () {return this.then(function save() {})});
+  });
+  afterEach(function () {
+    chai.spy.restore();
+  });
 
   it('should make a blank PDF', function () {
     return html2pdf('<div></div>').outputPdf().then(function (val) {
+      expect(this.save).to.have.been.called.once;
       comparePdf(val, 'blank.pdf');
     });
   });
@@ -13,6 +19,7 @@ describe('legacy mode', function () {
       jsPDF: {unit: 'in'}
     };
     return html2pdf('<h1>Margin: 1 inch</h1>', settings).outputPdf().then(function (val) {
+      expect(this.save).to.have.been.called.once;
       comparePdf(val, 'margin-1in.pdf');
     });
   });
