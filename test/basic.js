1284f7a40260b7c349c4a5fe82a390523246045b
Erik Koopmans <erik@erik-koopmans.com>
2019-07-09 23:16:22 +1000

    Remove top-level Mocha descriptor

--- test/basic.js
+++ test/basic.js
@@ -1,12 +1,10 @@
-describe('html2pdf', function () {
-  describe('creation', function () {
-    it('should exist', function () {
-      expect(html2pdf).to.exist;
-    });
+describe('creation', function () {
+  it('should exist', function () {
+    expect(html2pdf).to.exist;
+  });
 
-    it('should produce a thenable object', function () {
-      expect(html2pdf().then).to.be.a('function');
-      expect((new html2pdf.Worker).then).to.be.a('function');
-    });
+  it('should produce a thenable object', function () {
+    expect(html2pdf().then).to.be.a('function');
+    expect((new html2pdf.Worker).then).to.be.a('function');
   });
 });
