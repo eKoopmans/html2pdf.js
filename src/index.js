8f9d894d297ec1d65022321dae2cc7b760e5909d
Scot Murphy <scot@prohome.com>
2022-01-25 22:52:34 +0000

    Re-order the plugins to operate correctly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Worker from './worker.js';
 import './plugin/jspdf-plugin.js';
 import './plugin/pagebreaks.js';
-import './plugin/hyperlinks.js';
 import './plugin/ios-pdf-fix.js';
+import './plugin/hyperlinks.js';
 
 /**
  * Generate a PDF from an HTML element or string using html2canvas and jsPDF.
