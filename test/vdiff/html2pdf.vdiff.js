03a155d54279f3598d0bf53942a51e9d72e69db3
Erik Koopmans <erik@erik-koopmans.com>
2025-08-17 21:53:08 -0400

    Restructure vdiff file

--- test/vdiff/html2pdf.vdiff.js
+++ test/vdiff/html2pdf.vdiff.js
@@ -3,59 +3,59 @@ import { expect, fixture, html } from '@brightspace-ui/testing';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import sinon from 'sinon';
 
+const defaultSettings = { html2canvas: { logging: false } };
+const pageBreakSettings = pagebreak => ({
+  ...defaultSettings,
+  pagebreak,
+  jsPDF: { orientation: 'portrait', unit: 'in', format: 'letter' },
+});
+
+const conditions = {
+  default: {},
+  legacy: {
+    command: async (window, element, settings) => {
+      const stub = sinon.stub(window.html2pdf.Worker.prototype, 'save').callsFake(function () { return this.then(function save() { }); });
+      const arrayBuffer = await window.html2pdf(element, settings).outputPdf('arraybuffer');
+      stub.restore();
+      return arrayBuffer;
+    },
+  },
+  margin: {
+    settings: { ...defaultSettings, margin: 1, jsPDF: { unit: 'in' } },
+  },
+  selectCanvas: {
+    selector: '#canvas',
+  },
+  selectMainId: {
+    selector: '#main',
+  },
+  pagebreakLegacy: {
+    settings: pageBreakSettings({ mode: 'legacy' }),
+  },
+  pagebreakCss: {
+    settings: pageBreakSettings({ mode: 'css' }),
+  },
+  pagebreakAvoidAll: {
+    settings: pageBreakSettings({ mode: 'avoid-all' }),
+  },
+  pagebreakSpecify: {
+    settings: pageBreakSettings({ before: '.before', after: '.after', avoid: '.avoid' }),
+  },
+};
+
+const fileConditions = {
+  'blank': ['default'],
+  'lorem-ipsum': ['default', 'legacy', 'margin'],
+  'all-tags': ['default', 'selectCanvas'],
+  'css-selectors': ['default', 'selectMainId'],
+  'pagebreaks': ['pagebreakLegacy', 'pagebreakCss', 'pagebreakAvoidAll', 'pagebreakSpecify'],
+};
+
 describe('html2pdf', () => {
-  const defaultSettings = { html2canvas: { logging: false } };
-  const pageBreakSettings = pagebreak => ({
-    ...defaultSettings,
-    pagebreak,
-    jsPDF: { orientation: 'portrait', unit: 'in', format: 'letter' },
-  });
-
-  const conditions = {
-    default: {},
-    legacy: {
-      command: async (window, element, settings) => {
-        const stub = sinon.stub(window.html2pdf.Worker.prototype, 'save').callsFake(function () { return this.then(function save() { }); });
-        const arrayBuffer = await window.html2pdf(element, settings).outputPdf('arraybuffer');
-        stub.restore();
-        return arrayBuffer;
-      },
-    },
-    margin: {
-      settings: { ...defaultSettings, margin: 1, jsPDF: { unit: 'in' } },
-    },
-    selectCanvas: {
-      selector: '#canvas',
-    },
-    selectMainId: {
-      selector: '#main',
-    },
-    pagebreakLegacy: {
-      settings: pageBreakSettings({ mode: 'legacy' }),
-    },
-    pagebreakCss: {
-      settings: pageBreakSettings({ mode: 'css' }),
-    },
-    pagebreakAvoidAll: {
-      settings: pageBreakSettings({ mode: 'avoid-all' }),
-    },
-    pagebreakSpecify: {
-      settings: pageBreakSettings({ before: '.before', after: '.after', avoid: '.avoid' }),
-    },
-  };
-
-  const filesToTest = {
-    'blank': [ 'default' ],
-    'lorem-ipsum': [ 'default', 'legacy', 'margin' ],
-    'all-tags': [ 'default', 'selectCanvas' ],
-    'css-selectors': [ 'default', 'selectMainId' ],
-    'pagebreaks': [ 'pagebreakLegacy', 'pagebreakCss', 'pagebreakAvoidAll', 'pagebreakSpecify' ],
-  };
-
-  Object.keys(filesToTest).forEach(file => describe(file, () => {
+  Object.keys(fileConditions).forEach(file => describe(file, () => {
     const href = `/test/reference/${file}.html`;
 
-    filesToTest[file].forEach(conditionName => it(conditionName, async () => {
+    fileConditions[file].forEach(conditionName => it(conditionName, async () => {
       const condition = conditions[conditionName];
       const testHarness = await fixture(html`
         <test-harness
